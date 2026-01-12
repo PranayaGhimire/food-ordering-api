7254ccb47c84ab6041d7533d52a6c4050e641286
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2026-01-12 11:46:26 +0545

    Fix Module Error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from '../common/strategies/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/user.schema';
 
 @Module({
   imports: [
     UsersModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
